98d67701393ccd1bd91c122dfe7cc413440f7930
Ksenia Shepherd <111@mail.ru>
2021-10-25 10:58:43 +0300

    add testing with Junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,12 @@
 package ru.netology.service;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 
 public class CashbackHackServiceTest {
     private CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldCheckRemainder() {
         int expected = 100;
 
@@ -14,14 +14,14 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldAdviseBuyWhenCheckingBuyAmount() {
         String expected = "Для начисления кэшбека не хватает 100 рублей";
         String actual = service.checkingBuyAmount(900);
         assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldcheckingBuyAmountWithoutAdviseBuy() {
         String expected = null;
         String actual = service.checkingBuyAmount(3000);
